b97cc7bbb678315bd142fd2f2e39dcff49823979
sathya_sankar <sathya.shankar@rucksa.com>
2026-09-03 16:13:16 +0530

    Initial MoneyGoal website

---
+++ dist/assets/config.js
@@ -0,0 +1,5 @@
+window.MONEYGOAL_CONFIG = Object.freeze({
+  // Replace this empty value with the complete Alice Blue referral URL issued
+  // to The MoneyGoal. All partner CTAs across the site read from this setting.
+  aliceBlueReferralUrl: ""
+});
